c68f4a14f6297fe585cc1b9448285f10dc773d76
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-03-09 01:37:04 +0300

    Here is my Composite Pattern example.

---
+++ Structural_Patterns/Composite__Pattern/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite__Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Finally, I run and call my objects in my system sequentially.
+            //Actually, we can compare it to this.
+            //File/My Personal Files/Photo and Video/My Photos
+            //It is similar to the category logic in an e-commerce application.
+            //categories/tv,image and sound systems/television/brand/model
+
+            Official ceo = new Official("Oğuzhan", Position.CEO);
+
+
+            Official team_manager = new Official("Elif", Position.Team_Manager);
+            team_manager.AddEmployee(new SUB_AUTHORITY("Gamze", Position.Development));
+            team_manager.AddEmployee(new SUB_AUTHORITY("Levent", Position.Development));
+
+            Official hr_director = new Official("Büşra", Position.HR_Director);
+            hr_director.AddEmployee(new SUB_AUTHORITY("Okan", Position.Marketing));
+            hr_director.AddEmployee(new SUB_AUTHORITY("Sedef", Position.Architect));
+
+            ceo.AddEmployee(team_manager);
+            ceo.AddEmployee(hr_director);
+
+
+
+            ceo.SendMessage();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Structural_Patterns/Composite__Pattern/SUB_AUTHORITY.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite__Pattern
+{
+    //LEAF
+    public class SUB_AUTHORITY : Employee
+    {
+        //Although the 'SUB_AUTHORITY' class represents a developer, it still derives from the 'Employee'
+        //abstract class since it is essentially an employee and provides the necessary implementations in it.
+
+        public SUB_AUTHORITY(string name, Position position) : base(name, position)
+        {
+        }
+
+
+        public override void SendMessage()
+        {
+            Console.WriteLine($"{_position}-{_name}");
+        }
+    }
+}
