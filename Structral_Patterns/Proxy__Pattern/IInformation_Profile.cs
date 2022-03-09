09bb0787a1aad05466dac95f3a1237893509a95d
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-03-09 21:38:02 +0300

    Here is my Proxy Pattern example.

---
+++ Structral_Patterns/Proxy__Pattern/IInformation_Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy__Pattern
+{
+    public interface IInformation_Profile
+    {
+        void ShowProfil();
+
+
+    }
+}
