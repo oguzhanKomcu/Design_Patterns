6458f3fae64dcd8dbba95e10ba3009e797aa4ae0
oğuzhan kömcü <komcuoguzz@gmail.com>
2022-03-09 22:06:16 +0300

    Here is my Proxy Pattern example.

---
+++ Structural_Patterns/Proxy_Design_Pattern/Profile_Owner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Design_Pattern
+{
+    public class Profile_Owner : IInformation_Profile
+    {
+
+        private string username;
+        private string password;
+
+        public Profile_Owner(string username, string password)
+        {
+
+            this.username = username;
+            this.password = password;
+        }
+
+
+        public void ShowProfil()
+        {
+            Console.WriteLine($"The profile is opened and the profile content is displayed.\nUser Name: {username}");
+        }
+    }
+}
